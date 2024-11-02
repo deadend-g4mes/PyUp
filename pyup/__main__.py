33e1dde8bc79f1bb266d675ae5c623c62a6a2bab
deadend-g4mes <leighton.h.carpenter@gmail.com>
2024-11-02 15:49:33 -0500

    v0.1.0

--- pyup/__main__.py
+++ pyup/__main__.py
@@ -1,19 +1,33 @@
-import requests
-from requests import Response
+from requests import Response, Session
 from enum import Enum
+import re
+from datetime import datetime
 
+http = Session()
 class Type(Enum):
-    COIN = 'coin'
-    EXP = 'exp'
-    ITEM = 'item'
+  COIN = 'coin'
+  EXP = 'exp'
+  ITEM = 'item'
 
 class CountSetType(Enum):
-    ABSOLUTE = 'absolute'
-    RELATIVE = 'relative'
+  ABSOLUTE = 'absolute'
+  RELATIVE = 'relative'
         
 
-def call(call) -> Response:
-    return requests.get(url=call)
-    
-    
+def call(call,host,port) -> Response:
+  
+  url = 'http://' + host + ':' + port + '/api?' + call
+  return http.get(url=url)
+
+def color_check(hex: str):
+  match = re.seach(r'^#(?:[0-9a-fA-F]{3}){1,2}$', hex)
+
+  if match:                      
+    return True
+  else:
+    return False
+
+def time_to_epoch(date:datetime):
+
+  return int(date.timestamp()) * 1000
 
--- pyup/task.py
+++ pyup/task.py
@@ -1,146 +1,180 @@
 from .pyup import PyUp
 from .__main__ import *
 
-self: PyUp
+class Task(PyUp):
 
-def add_task(todo: str, notes: str = None, coin: int = None, coin_var: int = None, exp: int = None, skills: list = None, category: int = None, frequency: int = None, importance: int = None, difficulty: int = None, item_id: int = None, item_name: str = None, item_amount: int = None, deadline: int = None, color: str = None, background_url: str = None, frozen: bool = None, freeze_until: int = None, coin_penality_factor: float = None, exp_pentalty_factor: float = None, write_feelings: bool = None) -> Response:
-    '''
-    Creates a task.
-    
-    :param
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
+    def __init__(self, pyup:PyUp):
+        '''
+        Some api calls for communicating with task objects in LifeUp.
+        '''
+        super().__init__(pyup.host, pyup.port)
+        self.host = pyup.host
+        self.port = pyup.port
 
-    params = vars()
-
-    url = 'http://' + self.host + ':' + self.port + '/api?url=lifeup://api/add_task?'
-    for key, value in params.items():
-        if value is not None:
-            url = url + key + '=' + str(value) + '%26'
-        else:
-            continue
-    url = url[:-3]
-
-    result = call(call=url)
-
-    return result
-    
-def complete_task(id: int = None, gid: int = None, name: str = None, ui: bool = None, count: int = None, count_set_type: CountSetType = None, count_force_sum_up: bool = None, reward_factor: float = None) -> Response:
-    '''
-    Completes a task. Only unfinished tasks will be searched.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    if id or gid or name == None:
-        raise ValueError("OneOfType: id, gid or name must have a value.")
-    
-    params = vars()
-
-    url = 'http://' + self.host + ':' + self.port + '/api?url=lifeup://api/complete?'
-    for key, value in params.items():
-        if value is not None:
-            url = url + key + '=' + str(value) + '%26'
-        else:
-            continue
-    url = url[:-3]
-
-    result = call(call=url)
+    def add_task(self, todo: str, notes: str = None, coin: int = None, coin_var: int = None, exp: int = None, skills: list = None, category: int = None, frequency: int = None, importance: int = None, difficulty: int = None, item_id: int = None, item_name: str = None, item_amount: int = None, deadline: int = None, color: str = None, background_url: str = None, frozen: bool = None, freeze_until: int = None, coin_penality_factor: float = None, exp_pentalty_factor: float = None, write_feelings: bool = None) -> Response:
+        '''
+        Creates a task.
         
-    return result
-
-def give_up_task(id: int = None, gid: int = None, name: str = None) -> Response:
-
-    '''
-    Gives up a task.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    if id or gid or name == None:
-        raise ValueError("OneOfType: id, gid or name must have a value.")
-    
-    params = vars()
-
-    url = 'http://' + self.host + ':' + self.port + '/api?url=lifeup://api/complete?'
-    for key, value in params.items():
-        if value is not None:
-            url = url + key + '=' + str(value) + '%26'
-        else:
-            continue
-    url = url[:-3]
-
-    result = call(call=url)
+        :param
+        :return: :class:`Response <Response>` object
+        :rtype: requests.Response
+        '''
+        if not color_check(color):
+            raise ValueError("param: title_color_string: value must be a valid hex color code.")
         
-    return result
+        params = vars()
+        try:
+            params.pop('self')
+        finally:
+            pass
 
-def freeze_task(id: int = None, gid: int = None, name: str = None) -> Response:
+        url = 'url=lifeup://api/add_task?'
+        for key, value in params.items():
+            if value is not None:
+                url = url + key + '=' + str(value) + '%26'
+            else:
+                continue
+        url = url[:-3]
 
-    '''
-    Freezes a task, only for repeating tasks.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    if id or gid or name == None:
-        raise ValueError("OneOfType: id, gid or name must have a value.")
-    
-    params = vars()
+        result = call(call=url,host=self.host,port=self.port)
 
-    url = 'http://' + self.host + ':' + self.port + '/api?url=lifeup://api/complete?'
-    for key, value in params.items():
-        if value is not None:
-            url = url + key + '=' + str(value) + '%26'
-        else:
-            continue
-    url = url[:-3]
-
-    result = call(call=url)
+        return result
         
-    return result
+    def complete_task(self, id: int = None, gid: int = None, name: str = None, ui: bool = None, count: int = None, count_set_type: CountSetType = None, count_force_sum_up: bool = None, reward_factor: float = None) -> Response:
+        '''
+        Completes a task. Only unfinished tasks will be searched.
+        :return: :class:`Response <Response>` object
+        :rtype: requests.Response
+        '''
+        if id or gid or name == None:
+            raise ValueError("OneOfType: id, gid or name must have a value.")
         
-def unfreeze_task(id: int = None, gid: int = None, name: str = None) -> Response:
+        params = vars()
+        try:
+            params.pop('self')
+        finally:
+            pass
 
-    '''
-    Unfreezes a task, only for repeating tasks.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    if id or gid or name == None:
-        raise ValueError("OneOfType: id, gid or name must have a value.")
-    
-    params = vars()
+        url = 'url=lifeup://api/complete?'
+        for key, value in params.items():
+            if value is not None:
+                url = url + key + '=' + str(value) + '%26'
+            else:
+                continue
+        url = url[:-3]
 
-    url = 'http://' + self.host + ':' + self.port + '/api?url=lifeup://api/complete?'
-    for key, value in params.items():
-        if value is not None:
-            url = url + key + '=' + str(value) + '%26'
-        else:
-            continue
-    url = url[:-3]
+        result = call(call=url,host=self.host,port=self.port)
+            
+        return result
 
-    result = call(call=url)
+    def give_up_task(self, id: int = None, gid: int = None, name: str = None) -> Response:
+
+        '''
+        Gives up a task.
+        :return: :class:`Response <Response>` object
+        :rtype: requests.Response
+        '''
+        if id or gid or name == None:
+            raise ValueError("OneOfType: id, gid or name must have a value.")
         
-    return result
+        params = vars()
+        try:
+            params.pop('self')
+        finally:
+            pass
 
-def delete_task(id: int = None, gid: int = None, name: str = None) -> Response:
+        url = 'url=lifeup://api/give_up?'
+        for key, value in params.items():
+            if value is not None:
+                url = url + key + '=' + str(value) + '%26'
+            else:
+                continue
+        url = url[:-3]
 
-    '''
-    Deletes a task.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    if id or gid or name == None:
-        raise ValueError("OneOfType: id, gid or name must have a value.")
-    
-    params = vars()
+        result = call(call=url,host=self.host,port=self.port)
+            
+        return result
 
-    url = 'http://' + self.host + ':' + self.port + '/api?url=lifeup://api/complete?'
-    for key, value in params.items():
-        if value is not None:
-            url = url + key + '=' + str(value) + '%26'
-        else:
-            continue
-    url = url[:-3]
+    def freeze_task(self, id: int = None, gid: int = None, name: str = None) -> Response:
 
-    result = call(call=url)
+        '''
+        Freezes a task, only for repeating tasks.
+        :return: :class:`Response <Response>` object
+        :rtype: requests.Response
+        '''
+        if id or gid or name == None:
+            raise ValueError("OneOfType: id, gid or name must have a value.")
         
-    return result
+        params = vars()
+        try:
+            params.pop('self')
+        finally:
+            pass
+
+        url = 'url=lifeup://api/freeze?'
+        for key, value in params.items():
+            if value is not None:
+                url = url + key + '=' + str(value) + '%26'
+            else:
+                continue
+        url = url[:-3]
+
+        result = call(call=url,host=self.host,port=self.port)
+            
+        return result
+            
+    def unfreeze_task(self, id: int = None, gid: int = None, name: str = None) -> Response:
+
+        '''
+        Unfreezes a task, only for repeating tasks.
+        :return: :class:`Response <Response>` object
+        :rtype: requests.Response
+        '''
+        if id or gid or name == None:
+            raise ValueError("OneOfType: id, gid or name must have a value.")
+        
+        params = vars()
+        try:
+            params.pop('self')
+        finally:
+            pass
+
+        url = 'url=lifeup://api/unfreeze?'
+        for key, value in params.items():
+            if value is not None:
+                url = url + key + '=' + str(value) + '%26'
+            else:
+                continue
+        url = url[:-3]
+
+        result = call(call=url,host=self.host,port=self.port)
+            
+        return result
+
+    def delete_task(self, id: int = None, gid: int = None, name: str = None) -> Response:
+
+        '''
+        Deletes a task.
+        :return: :class:`Response <Response>` object
+        :rtype: requests.Response
+        '''
+        if id or gid or name == None:
+            raise ValueError("OneOfType: id, gid or name must have a value.")
+        
+        params = vars()
+        try:
+            params.pop('self')
+        finally:
+            pass
+
+        url = 'url=lifeup://api/delete_task?'
+        for key, value in params.items():
+            if value is not None:
+                url = url + key + '=' + str(value) + '%26'
+            else:
+                continue
+        url = url[:-3]
+
+        result = call(call=url,host=self.host,port=self.port)
+            
+        return result
